c1d05610e8e76908134cc129a59ef3916cf7030c
manan08 <72185965+manan08@users.noreply.github.com>
2025-11-16 23:30:22 +0530

    Adding Profile Update endpoint

--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { authUser } = require('../middlewares/auth');
+const { validateProfileUpdateData } = require('../utils/validations');
 
 // GET /user 
 router.get('/profile', authUser, async (req, res) => {
@@ -10,6 +11,28 @@ router.get('/profile', authUser, async (req, res) => {
     } catch (error) {
         console.error('Error fetching user details', error);
         res.status(500).send({ status: 'false', message: 'Error fetching user', data: [] })
+    }
+})
+
+// PATCH /profile/edit
+router.patch('/profile/edit', authUser, async (req, res) => {
+    try {
+        const isValidData = validateProfileUpdateData(req);
+
+        if (!isValidData) {
+            return res.status(400).json({ 'status': 'success', message: 'Invalid profile data' });
+        }
+
+        const user = req.user;
+
+        Object.keys(req.body).forEach(key => user[key] = req.body[key]);
+
+        await user.save();
+
+        return res.json({ 'status': 'success', message: 'Profile Updated Successfully', data: user });
+    } catch (error) {
+        console.log('Error in Edit Profile: ', error.message);
+        return res.status(500).json({ 'status': 'error', message: error.message, data: [] });
     }
 })
 
--- src/utils/validations.js
+++ src/utils/validations.js
@@ -7,6 +7,24 @@ const validateSignUpData = (req) => {
     }
 }
 
+const validateProfileUpdateData = (req) => {
+    try {
+        const ALLOWED_EDIT_FIELDS = ['firstName', 'lastName', 'emailId', 'age', 'gender'];
+
+        const isValidData = Object.keys(req.body).every(field => ALLOWED_EDIT_FIELDS.includes(field));
+
+        if (!isValidData) {
+            return false;
+        }
+
+        return true;
+    } catch (error) {
+        console.log("Error in Validating Data", error.message);
+        return false;
+    }
+}
+
 module.exports = {
-    validateSignUpData
+    validateSignUpData,
+    validateProfileUpdateData
 }
